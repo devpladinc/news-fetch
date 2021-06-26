6d63d522e014a3238ae72cc8bc15172f5309e8e4
devpladinc <dev.pladinc@gmail.com>
2021-06-26 14:37:13 +0800

    Revert "Jun26 init commit"
    This reverts commit 5c32b10f4a1b8d3c1c23ea873f866b467066021b.

--- configs.py
+++
@@ -1,5 +0,0 @@
-config = {
-    'BASE_URL' : "https://newsapi.org",
-    'API_KEY' : "df3eea665803416ca18a251315e32066",
-    'MONGO_STR' : "mongodb+srv://chadev:QMtwr9Vg756wx8K@cluster0.v13cn.mongodb.net/python-news?retryWrites=true&w=majority"
-}
--- main.py
+++
@@ -1,9 +0,0 @@
-from news_scraper import NewsScraper as NS
-
-def main():
-    #create bot fetcher
-    bot = NS()
-    bot.fetch_data()
-
-if __name__ == "__main__":
-    main()
